6bb76498de4edcb189b0342ed11fb1c322c7885f
Anna <pompeeva.anna@gmail.com>
2024-07-23 07:53:24 +0000

    fix import

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pytest
 from freezegun import freeze_time
 
-from app.main import Transaction, TransactionService, TransactionType
+from app.service import Transaction, TransactionService, TransactionType
 
 
 @freeze_time('2024-07-12 03:21:34')
