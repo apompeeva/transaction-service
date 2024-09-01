2295eb46dd947aa872baedb4e65b8384cc43d8a6
Anna <pompeeva.anna@gmail.com>
2024-09-01 13:36:17 +0700

    fix double quotes

--- src/app/endpoints/endpoints.py
+++ src/app/endpoints/endpoints.py
@@ -6,7 +6,7 @@ from app.schemas.schemas import ReportData, Transaction
 transaction_router = APIRouter()
 
 
-@transaction_router.post("/create_transaction", status_code=status.HTTP_200_OK)
+@transaction_router.post('/create_transaction', status_code=status.HTTP_200_OK)
 async def create_transaction(transaction: Transaction):
     """Создание транзакции."""
     transaction_result = await TransactionService.create_transaction(
@@ -17,11 +17,11 @@ async def create_transaction(transaction: Transaction):
     if transaction_result is None:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="User not verified",
+            detail='User not verified',
         )
 
 
-@transaction_router.post("/get_report", status_code=status.HTTP_200_OK)
+@transaction_router.post('/get_report', status_code=status.HTTP_200_OK)
 async def get_report(report_data: ReportData):
     """Получение отчета о транзакциях за период."""
     return await TransactionService.get_transaction(
@@ -31,7 +31,7 @@ async def get_report(report_data: ReportData):
     )
 
 
-@transaction_router.get("/healthz/ready", status_code=status.HTTP_200_OK)
+@transaction_router.get('/healthz/ready', status_code=status.HTTP_200_OK)
 async def health_check():
     """Проверка работоспособности сервиса."""
-    return "Ok"
+    return 'Ok'
